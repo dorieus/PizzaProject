88e016ae0494d460101b9e1f966e8035c2ad86ac
dorieus <56553692+dorieus@users.noreply.github.com>
2020-09-22 12:16:39 +0300

    delete basket function

--- index.js
+++ index.js
@@ -112,17 +112,3 @@ const close = document.querySelector(".close");
 close.addEventListener("click", () => {
   close.parentElement.parentElement.classList.add("hide");
 });
-
-$(document).ready(function () {
-  $(document).delegate(".open", "click", function (event) {
-    $(this).addClass("oppenned");
-    event.stopPropagation();
-  });
-  $(document).delegate("body", "click", function (event) {
-    $(".open").removeClass("oppenned");
-  });
-  $(document).delegate(".cls", "click", function (event) {
-    $(".open").removeClass("oppenned");
-    event.stopPropagation();
-  });
-});
